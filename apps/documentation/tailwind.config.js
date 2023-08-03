747e3916a34ec076da729a3698113fbf96d132f6
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2023-08-03 23:17:27 +0100

    docs: starting documentation site

---
+++ apps/documentation/tailwind.config.js
@@ -0,0 +1,21 @@
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['eudoxus sans', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
