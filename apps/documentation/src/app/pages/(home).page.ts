efab5914dc61037c3dd36f16adface071d5f515d
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2023-08-05 22:28:06 +0100

    docs: setting up analog

---
+++ apps/documentation/src/app/pages/(home).page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { AnalogWelcomeComponent } from './analog-welcome.component';
+
+@Component({
+  selector: 'documentation-home',
+  standalone: true,
+  imports: [AnalogWelcomeComponent],
+  template: ` <documentation-analog-welcome /> `,
+})
+export default class HomeComponent {}
